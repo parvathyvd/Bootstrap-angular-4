68c47f2032a4e3a9eef665cf5f47319db886f077
parvathyvd <parvathyvd@gmail.com>
2017-05-11 21:10:42 -0700

    Added a single page application using ngRoute

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,53 @@
+var myApp = angular.module('myApp', ['ngRoute']);
 
+myApp.config(function($routeProvider, $locationProvider) {
+            $locationProvider.hashPrefix('');
 
-$(function() {
-    $('.my-counting').countdown({
-        date: "June 7, 2027 15:03:26"
-    });
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+
 });
+
+myApp.service('nameService', function() {
+
+    var self = this;
+    this.name = 'John Doe';
+
+    this.namelength = function() {
+
+        return self.name.length;
+
+    };
+
+});
+
+myApp.controller('mainController', ['$scope', '$log','nameService', function($scope, $log, nameService) {
+
+    $scope.name = nameService.name;
+    $log.log(nameService.name);
+    $log.log(nameService.namelength());
+
+    $scope.$watch('name', function() {
+        nameService.name = $scope.name;
+    });
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log','nameService', function($scope, $log, nameService) {
+
+  $scope.name = nameService.name;
+
+  $scope.$watch('name', function() {
+        nameService.name = $scope.name;
+    });
+
+}]);
